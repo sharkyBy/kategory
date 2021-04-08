9524342395800600e098ef6f51b085efc3fb7ce8
sharkyBy <valik_1973@mail.ru>
2021-04-08 11:24:26 +0300

    add handler a add-button.TODO - not visibility value a input area

--- js/main.js
+++ js/main.js
@@ -1,34 +1,51 @@
 import {_store} from "./base.js";
 
-setMaterial(_store.teplo);
+
+// function load() {
+    setMaterial(_store.teplo); 
+    const firstSpan = document.getElementById("add");
+    firstSpan.addEventListener("click",(e)=> {        
+        getElem(e);
+    });
+
+    
+// }
 
 
+
+//Назначение события на кнопку первой строки
+// window.addEventListener("load", load)
+
+
+// Обработка события на кнопке
+const result = document.getElementById("result");
+result.addEventListener("click", getCellsMultiply)
+
+//Добавление строки в форму
 let addRow = document.getElementById("addRow");
 addRow.addEventListener("click", createRow);
 
-// let add = document.getElementsByName("add");
-// console.log(add)
-// add[0].addEventListener("click",()=>el.parentElement.remove())
-
-
+//Назначение значений новым полям option
 function setValue() {
     const select = document.getElementById("material");
  const res = {
     name:select.selectedOptions[0].innerText,
     val:select.selectedOptions[0].value
 }
+console.log(res);
 return res
 
 }
-
+// Установка значений в ячейки для строк
 function getElem(e) {
     let name = e.target.parentElement.parentElement.children[0].firstChild.value=setValue().name;
     let val = e.target.parentElement.parentElement.children[1].firstChild.value=setValue().val;
-    return name, val
+    // console.log("i'm entry")
+    return console.log(name)
 
 }
 
-
+// Создание элементов option и назначение значений
 function addElementOption(val, name) {
     let option = document.createElement("option");
     option.setAttribute("value",val);
@@ -37,6 +54,7 @@ function addElementOption(val, name) {
     return select.append(option)
 }
 
+//Загрузка значений для блока select
 function setMaterial(material) {
     return material.map((item) => {
         let val = item.heat;
@@ -46,11 +64,12 @@ function setMaterial(material) {
    
 }
 
+// Создание новой строки
 function createRow() {
     
     let table = document.getElementById("tableRow")
     let div = document.createElement("div");    
-    div.setAttribute("class","row");
+    setAttributes(div,{"class":"row", "name":"newRow"});
     let span = document.createElement("span");
         let icon = document.createElement("i");
         span.setAttribute("class","icon");
@@ -70,10 +89,25 @@ function createRow() {
     
 }
 
-
+//Установка атрибутов для элементов
 function setAttributes(el,attr) {
     for (let key in attr) {
         el.setAttribute(key,attr[key])
     }
+}
+
+function getCellsMultiply() {   
+    // console.log(rows.length);
+    let arrRows = Array.from(document.getElementsByName("newRow"));
+    let multiply = []
+    // console.log(arrRows)
+    arrRows.map( (item) => {
+        let heat = item.children[1].children[0].value;
+        let weight = item.children[2].children[0].value;
+        let res = heat * weight;
+        multiply.push(res);        
+    }  )
+
+    return console.log(multiply)
 }
 
