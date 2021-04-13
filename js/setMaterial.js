cafc97c4c73b909fc75468d8225c2503e0fde93b
sharkyBy <valik_1973@mail.ru>
2021-04-13 17:25:54 +0300

    added  sort function

--- js/setMaterial.js
+++ js/setMaterial.js
@@ -9,8 +9,30 @@ function addElementOption(val, name) {
     return select.append(option)
 }
 
+//Сортировка массива
+
+function sortMaterial(arr) {
+    
+    return  arr.sort( 
+        (a,b) => {
+           
+            if(a.name > b.name) {
+                return 1
+            } 
+            else if (a.name < b.name ) {
+                return -1
+            }
+             
+            else  {
+                return  0 
+        }
+        })
+   
+}
+
 //Загрузка значений для блока select
 function setMaterial(material) {
+    sortMaterial(material);
     return material.map((item) => {
         let val = item.heat;
         let name = item.name;
